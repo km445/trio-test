6cc243321f1a7257f6ac86656b4069e05bab61ff
km445 <km445@ukr.net>
2017-10-19 23:19:29 +0300

    make it better

--- main.py
+++ main.py
@@ -18,64 +18,60 @@ log=[]
 				
 @app.route("/", methods = ["GET", "POST"])
 def index():
-	data={"shop_id":config.shop_id, "shop_invoice_id":config.shop_invoice_id}
-	if request.method == "POST":
-		currency=request.form["currency"]
-		amount=request.form["amount"]
-		description=request.form["description"]
+	data={"shop_id":config.shop_id}
+	if request.method == "GET":
+		return render_template("main.html", log=log)
+	
+	elif request.method == "POST":		
+		currency = request.form["currency"]
+		amount = request.form["amount"]
+		description = request.form["description"]
 		try:
 			if float(amount)<0.01:
 				return render_template("main.html", 
-					error="Сумма должна быть более или равна 0.01", log=log)
+						error="Сумма оплаты должна быть более или равна 0.01", log=log)
 			else:
 				amount="%.2f" % float(amount)
 		except:
 			return render_template("main.html", 
-				error="Указана неправильная сумма оплаты.", log=log)
-		
+					error="Указана неправильная сумма оплаты.", log=log)
+		config.shop_invoice_id+=1
+		data.update({"shop_invoice_id":config.shop_invoice_id})
+		data.update({"amount":amount})
 		#case for currency USD
 		if currency == "USD":
-			url=config.url_tip
-			data["currency"]="840"
-			data["amount"]=amount
-			
+			data.update({"currency":"840"})
 			data["sign"]=generateHash(data)
 			log.append((config.shop_invoice_id, 
 						amount, 
 						currency, 
 						description, 
 						datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
-			config.shop_invoice_id+=1
+			
 			return render_template("form.html", 
-									url=url, 
+									url=config.url_tip, 
 									parameters=data, 
 									form_method="POST")
 		
 		#case for currency EUR	
 		elif currency == "EUR":
-			url=config.url_invoice
-			data["currency"]="978"
-			data["amount"]=amount
-			data["payway"]="payeer_eur"
+			data.update({"currency":"978", "payway":"payeer_eur"})
 			data["sign"]=generateHash(data)
 			data=json.dumps(data).encode("utf-8")
-			api_invoice_request=urllib.request.Request(url, data=data, headers={"Content-Type":"application/json"})
+			api_invoice_request=urllib.request.Request(config.url_invoice, data=data, headers={"Content-Type":"application/json"})
 			api_incoive_response=urllib.request.urlopen(api_invoice_request).read().decode("utf-8")
 			log.append((config.shop_invoice_id, 
 						amount, 
 						currency, 
 						description, 
 						datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
-			config.shop_invoice_id+=1
+
 			payment_request_form=json.loads(api_incoive_response)
 			
 			return render_template("form.html", 
 					url=payment_request_form["data"]["source"], 
 					parameters=payment_request_form["data"]["data"], 
 					form_method=payment_request_form["data"]["method"])
-			
-	elif request.method == "GET":
-		return render_template("main.html", log=log)
 
 if __name__=="__main__":
 	app.run(host="0.0.0.0", port=port, debug = False)
